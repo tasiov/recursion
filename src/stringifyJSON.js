47713f401f26f5e6b9f7ca7d935a4325e1b89dd7
Tasio Victoria <tasiovictoria@gmail.com>
2015-12-15 04:16:18 -0500

    Implemented stringifyJSON using recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,55 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = "";
+
+  function recurse(obj) {
+    if (obj === null) {
+      result += "null";
+      return;
+    } else if (typeof obj === "string") {
+      result += '"' + obj.toString() + '"';
+      return;
+    } else if (typeof obj === "number" || typeof obj === "boolean") {
+      result += obj.toString();
+      return;
+    }
+
+    if (Array.isArray(obj)) {
+      result += "[";
+      for (var i = 0; i < obj.length; i++) {
+        if (i > 0) {
+          result += ",";
+        }
+        recurse(obj[i]);
+      }
+      result += "]";
+    } else {
+      result += "{";
+      var first = true;
+      for (var i in obj) {
+        if (typeof obj[i] != "function" && typeof obj[i] != "undefined") {
+          if (first === false) {
+            result += ",";
+            first = false;
+          }
+          result += '"' + i + '":';
+          recurse(obj[i]);
+          first = false;
+          }
+      }
+      result += "}";
+    }
+  };
+
+  recurse(obj);
+
+  return result;
 };
+
+
+  // var result = "";
+
+  // result = recurse(obj);
+  // return result;
+
